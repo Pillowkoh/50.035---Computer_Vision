09d7980b18b6ee859e2ec4205ab78e6c58e8c6f7
Pillowkoh <72456637+Pillowkoh@users.noreply.github.com>
2022-03-22 17:57:23 +0800

    Updated ConvNetwork.ipynb

--- Problem_Set_2/week6/libs/layers.py
+++ Problem_Set_2/week6/libs/layers.py
@@ -89,7 +89,7 @@ def relu_forward(x):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    out = np.maximum(x,0)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -117,7 +117,8 @@ def relu_backward(dout, cache):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    dx = dout
+    dx[x<0] = 0
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
